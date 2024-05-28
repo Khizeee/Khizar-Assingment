85286fe16328739ec69a4b087742f8b42793470d
Khizeee <130128945+Khizeee@users.noreply.github.com>
2024-05-29 03:58:47 +0500

    Add files via upload

---
+++ index.dart
@@ -0,0 +1,26 @@
+
+
+import 'dart:ffi';
+
+void main(){
+
+
+  int number=2;
+  print(number);
+  /// ?
+  int? age;
+  print(age);
+  /// Const
+  const name="khizar";
+  print(name);
+  ///late
+  late int description;
+  description=12;
+  print(description);
+  ///Final
+  final nickname="kikki";
+  print(nickname);
+  ///double
+  const double num=1;
+  print(num);
+}
